0f0fbdc820d58421ba96b76d1d3c36f5bca8416e
Melvin <melvinkusters0@gmail.com>
2018-11-05 12:27:09 +0100

    Motiv client integration.
    Integration of first working commit.

---
+++ client/src/main/java/com/motiv/client/UserController.java
@@ -0,0 +1,43 @@
+package com.motiv.client;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@EnableOAuth2Sso
+@Configuration
+@RestController
+public class UserController extends WebSecurityConfigurerAdapter  {
+    // private static UserLogic userLogic;
+
+    /*
+    public UserController()
+    {
+        this.userLogic = Factory.getInstance().getUserLogic();
+    } */
+
+    // Temporary exposed user details about authenticated user for development purposes
+    @RequestMapping("/user")
+    public Principal principal(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
